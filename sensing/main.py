70715a97d680520e63a155aaa952eb4ccf6979e8
Nilp Amin <nilpamin2@gmail.com>
2022-03-20 01:34:11 +1000

    cleaned code

--- sensing/main.py
+++ sensing/main.py
@@ -5,13 +5,15 @@ import time
 
 LDR_THRESHOLD = 2000 
 
-green_led = Pin(25, Pin.OUT)
-laser = Pin(0, Pin.OUT)
-ldr = ADC(28) 
 
 def hardware_init():
-    laser.on()
+    global ldr
+    global green_led
+
+    ldr = ADC(28) 
+    green_led = Pin(25, Pin.OUT)
     green_led.off()
+
 
 def average_ldr(samples):
     total = 0
