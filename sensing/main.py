f5e59bfba121b6326ebdb73b688c15f5d2957508
nilp amin <s4581164@student.uq.edu.au>
2022-03-20 00:58:21 +1000

    inital commit

---
+++ sensing/main.py
@@ -0,0 +1,32 @@
+#usr/bin/etc python3
+
+from machine import Pin, ADC
+import time
+
+LDR_THRESHOLD = 8000
+
+green_led = Pin(25, Pin.OUT)
+laser = Pin(0, Pin.OUT)
+ldr = ADC(28) 
+
+def hardware_init():
+    laser.on()
+    green_led.off()
+
+def average_ldr(samples):
+    total = 0
+    for _ in range(samples):
+        total = total + ldr.read_u16()
+
+    return total / samples 
+
+if __name__ == "__main__":
+    hardware_init()
+
+    while True:
+        ldr_reading = average_ldr(50)
+        if ldr_reading < LDR_THRESHOLD:
+            print(ldr_reading)
+            green_led.on()
+        else:
+            green_led.off()
