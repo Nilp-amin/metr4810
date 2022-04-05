fb6f8542d28f7ccd33ad89521009c624e8841b82
Nilp Amin <nilpamin2@gmail.com>
2022-04-05 23:31:12 +1000

    added code for vl530l0x

--- sensing/main.py
+++ sensing/main.py
@@ -1,7 +1,8 @@
 #usr/bin/etc python3
 
-from machine import Pin, ADC
-import time
+from machine import Pin, ADC, I2C
+
+from vl53l0x import VL53L0X
 
 LDR_THRESHOLD = 2000 
 
@@ -9,8 +10,13 @@ LDR_THRESHOLD = 2000
 def hardware_init():
     global ldr
     global green_led
+    global vl53l0x
 
     ldr = ADC(28) 
+
+    i2c = I2C(0, scl=Pin(1), sda=Pin(0))
+    vl53l0x = VL53L0X(i2c)
+
     green_led = Pin(25, Pin.OUT)
     green_led.off()
 
@@ -22,9 +28,7 @@ def average_ldr(samples):
 
     return total / samples 
 
-if __name__ == "__main__":
-    hardware_init()
-
+def test_ldr():
     while True:
         ldr_reading = average_ldr(50)
         if ldr_reading < LDR_THRESHOLD:
@@ -32,3 +36,12 @@ if __name__ == "__main__":
             green_led.on()
         else:
             green_led.off()
+
+def test_tof():
+    while(1):
+        print("Hello world")
+
+if __name__ == "__main__":
+    hardware_init()
+
+    test_tof()
